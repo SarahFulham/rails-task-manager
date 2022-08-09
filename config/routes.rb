94a8cc95c445ddf9473f5ce553b02230c8061e96
sarah fulham <sarahlfulham@gmail.com>
2022-08-09 13:54:29 -0400

    new and show

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # Read All
   get '/tasks', to: 'tasks#index'
+
+  # Create
+  get '/tasks/new', to: 'tasks#new', as: :new_task
+  post '/tasks', to: 'tasks#create'
+
+  # Read One
+  get '/tasks/:id', to: 'tasks#show', as: :task
 end
